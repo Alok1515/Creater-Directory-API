c670f2b4867ad255ec46290cfd2074492aba97b3
Alok Kumar <alok@example.com>
2026-07-05 16:05:30 +0530

    add main Spring Boot application entry point

---
+++ src/main/java/com/creator/CreatorDirectoryApplication.java
@@ -0,0 +1,12 @@
+package com.creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatorDirectoryApplication.class, args);
+    }
+}
